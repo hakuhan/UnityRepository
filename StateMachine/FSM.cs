e8d415195ecfee24d1177c29e79f43ee5993f816
HakuHayabusa <bh1981314@gmail.com>
2018-05-24 18:37:31 +0800

    Add State machine component with an example

---
+++ StateMachine/FSM.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameCore {
+
+public class FSM : MonoBehaviour {
+
+	protected State m_currentState = new State();
+	
+	/************************************************/
+	// Use this for initialization
+	protected void Start () {
+		
+	}
+
+	/************************************************/
+	
+	// Update is called once per frame
+	protected void Update () {
+		// Debug.Log("FSM update!");
+		m_currentState.Update();
+	}
+
+	/************************************************/
+	protected void SwichState(State newState)
+	{
+		// go to end state
+		m_currentState.End();
+		
+		m_currentState = newState;
+
+		// Turn to next begin state
+		m_currentState.Begin();
+	}
+
+	/************************************************/
+
+	public bool IsCurrentState(State state)
+	{
+		return state == m_currentState;
+	}	
+
+	/************************************************/
+
+	public State GetCurrentState()
+	{
+		return m_currentState;
+	}
+
+	/************************************************/	
+}
+
+}
